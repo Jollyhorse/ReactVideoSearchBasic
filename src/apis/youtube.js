8a20a0c4b542961ebaa992c3377641d2ba13ae52
Brian Burnett <b-burnett@cox.net>
2020-04-15 01:07:21 -0700

    update Video Item

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-const YOUTUBEKEY = "AIzaSyD7LR80eoW290G5_x36UoP2yQQaAl6-UW4";
-
 export default axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    type: "video",
-    maxResults: 5,
-    key: YOUTUBEKEY
-  }
+  baseURL: "https://www.googleapis.com/youtube/v3"
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
+const YOUTUBEKEY = "AIzaSyD7LR80eoW290G5_x36UoP2yQQaAl6-UW4";
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    console.log(term);
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: YOUTUBEKEY
+      }
+    });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
